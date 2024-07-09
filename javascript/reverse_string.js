497602f14393e4655bd32e537186165d92abcbec
shadowsnavi <kumori.k.kuro@gmail.com>
2024-07-09 15:03:41 -0700

    complete lab

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
-  // type your code here
+
+  const array=[]
+  for(let i = str.length - 1; i>=0; i--){
+    array.push(str[i])
+  }
+  return array.join('')
+
 }
 
 if (require.main === module) {
@@ -17,3 +23,10 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+  1. create new empty
+  2. count length of string
+  3. reverse the orginal array
+  4. store and save revesed in new array
+  5. return new array
+*/
